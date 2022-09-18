05d3c9be6d6d646c547afa6889d42818ed48adc0
Busra Gunes <busraaguunes@gmail.com>
2022-09-18 15:33:24 +0300

    news collection view project

---
+++ newsProject/View/NewsCollectionViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  NewsCollectionViewCell.swift
+//  newsProject
+//
+//  Created by Büşra Güneş on 14.09.2022.
+//
+
+import UIKit
+
+class NewsCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var newsImageView: UIImageView!
+    @IBOutlet weak var newsLabel: UILabel!
+    @IBOutlet weak var viewDetailButton: UILabel!
+    
+    func setItem(item:NewsListModel){
+        newsImageView.image = item.newsImage
+        newsLabel.text = item.newsTitle
+        newsImageView.layer.cornerRadius = 15
+        viewDetailButton.layer.cornerRadius = 8
+    }
+    
+}
+
+
